ae04b3b8195a76b778ad8f055b1ee9e696ecc903
EC2 Default User <ec2-user@ip-10-16-10-228.us-west-2.compute.internal>
2023-06-15 15:22:38 +0000

    initial commit

---
+++ collections.py
@@ -0,0 +1,22 @@
+print("Hello, world")
+print("Python has three numeric types: int, float, and complex")
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
